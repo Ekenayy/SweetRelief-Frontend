73a50b2f7420d3af8c4e4d33071102d357b4ca38
Ekenayy <ekenayy@gmail.com>
2021-04-23 13:06:41 -0400

    map added and working

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <MapView 
+        style={styles.map}
+        showsUserLocation={true}
+        provider={PROVIDER_GOOGLE}
+      /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +24,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
