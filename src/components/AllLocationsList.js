a84dc58466ad7f86f9026b20b7c7ddbff0360f27
Ekenayy <ekenayy@gmail.com>
2021-05-03 19:26:18 -0400

    included some comments

--- src/components/AllLocationsList.js
+++ src/components/AllLocationsList.js
@@ -7,7 +7,9 @@ function AllLocationsList ({selectedLocation, setSelectedLocation, filters}) {
 
     const {locations} = React.useContext(LocationContext)
     const [contextLocations, setContextLocations] = locations
-    console.log(filters)
+
+    // Given the paramters that comes back from the filter bar, I want to filter these locations
+    // I need to do it by their attributes but I don't want to hardcode each one in
 
     const twentyFiveLocations = contextLocations.slice(0, 25).map((location) => {
         return <LocationItem key={location.id} setSelectedLocation={setSelectedLocation} location={location} />
