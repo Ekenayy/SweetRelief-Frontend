b9c95b630c7f0b242e3e31c718421b5d3ec1d1c4
Ekenayy <ekenayy@gmail.com>
2021-05-10 17:39:35 -0400

    psudochode

--- src/pages/CreateLocation.js
+++ src/pages/CreateLocation.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 function CreateLocation () {
 
+    // Have an empty MapView component with just one draggable marker
+    // The mapview should be zoomed in with the currentUser's location as the region
+    // Get the latitude and longitude from that marker
+    // Have the user provide an address / description
+    // Have the user fill out locType, free, unisex etc using sliders
+    // At the end include a button that captures this data and creates this location in the database
+    // Use setLocations to add the new location to the locations context 
 
 }
 
