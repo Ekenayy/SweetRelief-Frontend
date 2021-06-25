2f0cbcb82125497b6dc456a86425f317d62306d9
Ekenayy <ekenayy@gmail.com>
2021-06-25 11:44:49 -0400

    fixed memory leak problem that was connected to the bigwrapper on the main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -59,11 +59,13 @@ function Main ( {currentUser, navigation, setLoggedIn, setCurrentUser, setToken}
 
 
     return (
-        <BigWrapper>
+        // <BigWrapper>
+        <>
             <MapContainer filterBy={filterBy} setFilterBy={setFilterBy} wholeMap={wholeMap} handlePress={setAndFitToCoords} selectedLocation={selectedLocation} setSelectedLocation={setSelectedLocation}/>
             <NavBar navigation={navigation} setToken={setToken} setLoggedIn={setLoggedIn} setCurrentUser={setCurrentUser} setModalContent={setModalContent} modalContent={modalContent} filterBy={filterBy} setFilterBy={setFilterBy} currentUser={currentUser} setComments={setComments} comments={comments}  modalVisible={modalVisible} setModalVisible={setModalVisible} handlePress={setAndFitToCoords} selectedLocation={selectedLocation} setSelectedLocation={setSelectedLocation} />
             {modalVisible ? <ShowModal modalContent={modalContent} setModalContent={setModalContent} setComments={setComments} comments={comments} currentUser={currentUser} modalVisible={modalVisible} selectedLocation={selectedLocation} setModalVisible={setModalVisible} /> : null}
-        </BigWrapper>
+        </>
+        // </BigWrapper>
     )
 }
 
