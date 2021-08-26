28b9ae51fe5be3e8b95981fec95f3fd72ffb3738
Ekenayy <ekenayy@gmail.com>
2021-08-26 00:05:09 -0400

    created basic functions for fetching payment intent from db

--- src/components/PayScreen.js
+++ src/components/PayScreen.js
@@ -1,6 +1,6 @@
 import React, {useRef, useState, useEffect} from 'react' 
 import styled from 'styled-components'
-import { Text, DarkText, Wrapper, Span, H2, CloseView, CloseText} from '../styles/Styles'
+import { Text, DarkText, Wrapper, Span, H2, CloseView, PurpButton, CloseText} from '../styles/Styles'
 import { Foundation } from '@expo/vector-icons';
 import { CardField, useStripe, CardForm } from '@stripe/stripe-react-native';
 import {View} from 'react-native'
@@ -17,7 +17,7 @@ function PayScreen( { navigation, modalVisible, setModalContent, setModalVisible
 
     const ModalHolder = styled.View`
         flex: 1;
-        margin-top: 200px;
+        margin-top: 300px;
         width: 90%;
         align-self: center;
     `
@@ -32,6 +32,16 @@ function PayScreen( { navigation, modalVisible, setModalContent, setModalVisible
     const TitleText = styled(DarkText)`
         font-size: 24px;
     `
+    useEffect(() => {
+        // If the user doesn't have a stripe_id, then send a request to make an account
+        // If they do then don't do anything.
+        if (!currentUser.stripe_user_id) {
+            createAccount()
+        } else {
+            // console.log(stripe_user_id)
+        }
+
+    }, [])
 
     const createAccount = () => {
         fetch(`${BASE_URL}/create_user_stripe_account/${currentUser.id}`, {
@@ -49,16 +59,26 @@ function PayScreen( { navigation, modalVisible, setModalContent, setModalVisible
             })
     }
 
-    useEffect(() => {
-        // If the user doesn't have a stripe_id, then send a request to make an account
-        // If they do then don't do anything.
-        if (!currentUser.stripe_user_id) {
-            createAccount()
-        } else {
-            // console.log(stripe_user_id)
-        }
+    const fetchPaymentIntentClientSecret = async () => {
+        const response = await fetch(`${API_URL}/create_payment_intent`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                currency: 'usd',
+            }),
+            });
+            const {clientSecret} = await response.json();
+        
+            return clientSecret;
+        };
 
-    }, [])
+    const handlePayPress = async () => {
+        
+        const clientSecret = await fetchPaymentIntentClientSecret();
+    }
+
 
     return (
         <ModalHolder>
@@ -74,14 +94,24 @@ function PayScreen( { navigation, modalVisible, setModalContent, setModalVisible
                         }}
                         cardStyle={{
                             backgroundColor: '#191818',
-                            textColor: '#000000',
+                            textColor: '#ffffff',
                         }}
                         style={{
                             width: '100%',
-                            height: 50,
+                            height: 70,
                             marginVertical: 30,
                         }}
+                        onCardChange={(cardDetails) => {
+                            console.log('cardDetails', cardDetails);
+                        }}
+                        onFocus={(focusedField) => {
+                            console.log('focusField', focusedField);
+                        }}
+                        // onBlur={(obj) => console.log(obj)}
                     />
+                    <PurpButton>
+                        <Span>Pay</Span>
+                    </PurpButton>
             </ModalForm>
         </ModalHolder>
     )
