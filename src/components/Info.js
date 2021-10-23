bfaf033c0754974b68612130e7f2936f7b050220
Ekenayy <ekenayy@gmail.com>
2021-10-23 19:46:53 -0400

    changed color of info container

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,16 +1,21 @@
 import React, {useState}  from 'react'
 import { Text, DarkText, Wrapper, Span, H2, Modal, ModalHolder, CloseView, WebHead, WebViewCon, CloseText } from '../styles/Styles'
 import { Foundation } from '@expo/vector-icons';
+import styled from "styled-components";
+
 
 function Info ( {setModalVisible, modalVisible, setModalContent} ) {
 
+    const BigView = styled(WebViewCon)`
+        background-color: 'rgba(52, 52, 52, 0.85)';
+    `
     return (
-        <WebViewCon>
+        <BigView>
             <CloseView onPress={() => setModalVisible(false)}>
                 <Foundation name="x" size={24} color="black" />
             </CloseView>
             <Text>Hello from Info</Text>
-        </WebViewCon>
+        </BigView>
     )
 }
 
