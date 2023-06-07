2c7a7b6d3092b27ecd08e3efd49df0951eab1a85
ekene <ekene@athleticsnyc.com>
2023-06-07 00:27:48 -0600

    consolidate auth into one component

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,24 +1,19 @@
-import React, {useState, useEffect} from 'react'
-import {useForm} from 'react-hook-form';
+import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form';
 import { BASE_URL } from '@env'
 import styled from 'styled-components';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Alert, ActivityIndicator } from 'react-native'
+import { Alert, ActivityIndicator } from 'react-native'
 import { Input, Span, BrownButton, DarkText, Form } from '../styles/Styles'
 
-const H1 = styled.Text`
-font-size: 24px;
-color: blue;
-`
-
 const TitleView = styled.View`
-padding:12px;
-margin-bottom:0px;
+  padding:12px;
+  margin-bottom:0px;
 `
 
 const FormTitle = styled.Text`
-font-size: 24px;
-color: #F7F8F3;
+  font-size: 24px;
+  color: #F7F8F3;
 `
 
 const SubmitBtn = styled(BrownButton)`
@@ -26,30 +21,29 @@ const SubmitBtn = styled(BrownButton)`
 `
 
 const ErrorSpan = styled(Span)`
-color: red;
+  color: red;
 `
 
 const Body = styled.View`
-background: #BEA7E5;
-flex: 1;
+  background: #BEA7E5;
+  flex: 1;
 `
 const LoginView = styled.TouchableOpacity`
-align-self: center;
-margin-top: 15px;
+  align-self: center;
+  margin-top: 15px;
 `
 
 const ResetView = styled(LoginView)`
-margin-top: 30px;
+  margin-top: 30px;
 `
 
-function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
+function Auth ( {setCurrentUser, handleSkip, currentUser} ) {
 
   const {register, handleSubmit, setValue} = useForm();
   const [errors, setErrors] = useState("");
   const [attempts, setAttempts] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [content, setContent] = useState("Login");
-  const [userCreated, setUserCreated] = useState(false);
   const [userEmail, setUserEmail] = useState(null);
 
   useEffect(() => {
@@ -59,13 +53,22 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
     register('email_token')
   }, [register])
 
+  useEffect(() => {
+    if (currentUser && !currentUser.email_verified) {
+      setContent("Verify Email");
+      setUserEmail(currentUser.email);
+      sendEmailVerification(currentUser, false);
+      Alert.alert(`Verify your email! Please check your inbox at ${currentUser.email} for a verification token.`);
+    }
+  }, [currentUser])
+
   const handleLogin = data => {
     setIsLoading(true);
     setErrors("")
 
     let formBody = {
-        email: data.email.toLowerCase(),
-        password: data.password
+      email: data.email.toLowerCase(),
+      password: data.password
     }
 
     fetch(`${BASE_URL}/login`, {
@@ -93,9 +96,9 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
     setIsLoading(true);
 
     let formBody = {
-        name: data.name,
-        email: data.email.toLowerCase(),
-        password: data.password
+      name: data.name,
+      email: data.email.toLowerCase(),
+      password: data.password
     }
 
     fetch(`${BASE_URL}/register`, {
@@ -108,8 +111,6 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
         if (newUser.errors) {
             setErrors(newUser.errors);
         } else {
-            // setCurrentUser(newUser.user);
-            setUserCreated(true);
             sendEmailVerification(newUser.user);
             saveData(newUser.token);
             setUserEmail(newUser.user.email);
@@ -118,7 +119,7 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
       })
   }
 
-  const sendEmailVerification = (newUser) => {
+  const sendEmailVerification = (newUser, withAlert = true ) => {
 
     fetch(`${BASE_URL}/send_email_verification`, {
       method: 'POST',
@@ -132,17 +133,20 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
         if (newUser.errors) {
             setErrors(newUser.errors);
         } else {
-            Alert.alert(newUser.success);
+            if (withAlert) {
+              Alert.alert(newUser.success);
+            }
             setContent("Verify Email");
         }
       })
   }
 
   const onVerificationSubmit = data => {
+    setIsLoading(true);
 
     let formBody = {
-        token: data.email_token,
-        email: userEmail
+      token: data.email_token,
+      email: userEmail
     }
 
     fetch(`${BASE_URL}/verify_email`, {
@@ -158,7 +162,28 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
             setCurrentUser(newUser.user)
             Alert.alert(newUser.success)
         }
+        setIsLoading(false);
       })
+  }
+
+  const handleResetPass = data => {
+    setIsLoading(true);
+    fetch(`${BASE_URL}/check_email`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email: data.email.toLowerCase()})
+    })
+      .then(r => r.json())
+      .then(data => {
+        if (data.error) {
+            setErrors(data.error)
+        } else {
+            Alert.alert("Success! Password reset email sent. Check your spam folder if you don't see it in your inbox" )
+            setContent("Login");
+        }
+        setIsLoading(false);
+      })
+    setErrors(null)
   }
 
   const saveData = async (thisToken) => {
@@ -180,12 +205,14 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
             placeholder="First and Last Name"
             onChangeText={text => setValue('name', text)} 
             autoCorrect={false}
+            onFocus={() => setErrors("")}
           /> 
         }
         { content != "Verify Email" && 
           <Input 
             placeholder="Email"
             onChangeText={text => setValue('email', text)}
+            onFocus={() => setErrors("")}
           />
         }
         { (content == "Sign Up" || content == "Login") && 
@@ -193,6 +220,7 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
             placeholder="Passsword"
             secureTextEntry={true}
             onChangeText={text => setValue('password', text)}
+            onFocus={() => setErrors("")}
           />
         }
         { content == "Verify Email" && 
@@ -200,6 +228,7 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
           placeholder="Verification Code"
           onChangeText={text => setValue('email_token', text)}
           autoCorrect={false}
+          onFocus={() => setErrors("")}
         />
         }
         {errors ? <ErrorSpan>{errors}</ErrorSpan> : null}
@@ -218,6 +247,11 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
             {isLoading ? <ActivityIndicator size="large"/> : <Span>Verify</Span> }
           </SubmitBtn>
         }
+        { content == "Reset Password" && 
+          <SubmitBtn onPress={handleSubmit(handleResetPass)}>
+            <Span>Send email</Span>
+          </SubmitBtn>
+        }
         {content == 'Login' && <LoginView onPress={() => setContent('Sign Up')}>
             <DarkText>Don't have an account yet? Sign up</DarkText>
         </LoginView>}
@@ -227,7 +261,7 @@ function Auth ( {navigation, setCurrentUser, handleSkip, currentUser} ) {
         <LoginView onPress={() => handleSkip('Login')}>
             <DarkText>Skip login for now</DarkText>
         </LoginView>
-        {attempts >= 3 ? <ResetView onPress={() => navigation.navigate('Reset Password')}>
+        {attempts >= 3 && content !== "Reset Password" ? <ResetView onPress={() => setContent('Reset Password')}>
             <DarkText>Forgot your password?</DarkText>
         </ResetView> : null}
         </Form>  
